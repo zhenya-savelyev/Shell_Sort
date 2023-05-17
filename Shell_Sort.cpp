abafe6996ad1cceb3166bc266ded75e9212c9743
zhenya-savelyev <zheya.savelyev@gmail.com>
2023-05-17 20:17:05 +0300

    Add .gitattributes and .gitignore.

---
+++ Shell_Sort.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <vector>
+
+// Функция для сортировки массива методом Шелла
+void ShellSort(std::vector<int>& array) {
+    int n = array.size();
+
+    for (int gap = n / 2; gap > 0; gap /= 2) {
+        for (int i = gap; i < n; ++i) {
+            int temp = array[i];
+            int j;
+
+            for (j = i; j >= gap && array[j - gap] > temp; j -= gap) {
+                array[j] = array[j - gap];
+            }
+
+            array[j] = temp;
+        }
+    }
+}
+
+int main() {
+    std::vector<int> array;
+
+    int size;
+    std::cout << "Введите размер массива: ";
+    std::cin >> size;
+
+    std::cout << "Введите элементы массива: ";
+    for (int i = 0; i < size; ++i) {
+        int element;
+        std::cin >> element;
+        array.push_back(element);
+    }
+
+    ShellSort(array);
+
+    std::cout << "Отсортированный массив: ";
+    for (const auto& element : array) {
+        std::cout << element << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
